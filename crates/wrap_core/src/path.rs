91095e90fd4666d05cdbc26919c4491ca70d4963
Bayn-Web <Bayn1404@gmail.com>
2024-08-28 11:11:20 +0800

    test: add current path case with js and ts&fix: handle current path case miss ./ attribute

--- crates/wrap_core/src/path.rs
+++ crates/wrap_core/src/path.rs
@@ -5,15 +5,19 @@ pub fn get_language_source_relative_path(file_path: &PathBuf, language_path: &Pa
     let parent_dir = file_path.parent();
     let mut result = PathBuf::new();
     if let Some(parent) = parent_dir {
-        let relative_path = language_path.related_to(parent).unwrap();
-        if let Some(extension) = relative_path.extension() {
+        let path = language_path.related_to(parent).unwrap();
+        if !path.starts_with("../") {
+            result = PathBuf::from("./");
+        }
+        if let Some(extension) = path.extension() {
+            // 去除ts扩展名
             if extension == std::ffi::OsStr::new("ts") {
-                if let Some(parent) = relative_path.parent() {
-                    let file_stem = relative_path.file_stem().unwrap();
-                    result = PathBuf::from(parent).join(file_stem)
+                if let Some(parent) = path.parent() {
+                    let file_stem = path.file_stem().unwrap();
+                    result = result.join(parent).join(file_stem)
                 }
             } else {
-                result = PathBuf::from(relative_path);
+                result = result.join(path);
             }
         }
     }
@@ -28,7 +32,9 @@ mod tests {
     #[test]
     fn language_source_ts_test() {
         let file_path = PathBuf::from("/Users/yangguodong/learn/i18n-wrap-rust/test_projects/temp/event/src/views/audit/It/index.vue");
-        let language_path = PathBuf::from("/Users/yangguodong/learn/i18n-wrap-rust/test_projects/temp/event/src/languages/app.ts");
+        let language_path = PathBuf::from(
+            "/Users/yangguodong/learn/i18n-wrap-rust/test_projects/temp/event/src/languages/app.ts",
+        );
         let relative_path = get_language_source_relative_path(&file_path, &language_path);
         println!("Relative Path: {}", relative_path.display());
         assert_eq!(relative_path, PathBuf::from("../../../languages/app"));
@@ -48,6 +54,31 @@ mod tests {
         let language_path = PathBuf::from("/Users/yangguodong/learn/i18n-wrap-rust/test_projects/temp/event/src/languages/language.js");
         let relative_path = get_language_source_relative_path(&file_path, &language_path);
         println!("Relative Path: {}", relative_path.display());
-        assert_eq!(relative_path, PathBuf::from("../../../languages/language.js"));
+        assert_eq!(
+            relative_path,
+            PathBuf::from("../../../languages/language.js")
+        );
+    }
+
+    #[test]
+    fn language_source_outer_test() {
+        let file_path = PathBuf::from(
+            "/Users/yangguodong/learn/i18n-wrap-rust/test_projects/temp/event/src/index.vue",
+        );
+        let language_path = PathBuf::from("/Users/yangguodong/learn/i18n-wrap-rust/test_projects/temp/event/src/languages/language.js");
+        let relative_path = get_language_source_relative_path(&file_path, &language_path);
+        println!("Relative Path: {}", relative_path.display());
+        assert_eq!(relative_path, PathBuf::from("./languages/language.js"));
+    }
+
+    #[test]
+    fn language_source_outer_ts_test() {
+        let file_path = PathBuf::from(
+            "/Users/yangguodong/learn/i18n-wrap-rust/test_projects/temp/event/src/index.vue",
+        );
+        let language_path = PathBuf::from("/Users/yangguodong/learn/i18n-wrap-rust/test_projects/temp/event/src/languages/language.ts");
+        let relative_path = get_language_source_relative_path(&file_path, &language_path);
+        println!("Relative Path: {}", relative_path.display());
+        assert_eq!(relative_path, PathBuf::from("./languages/language"));
     }
 }
